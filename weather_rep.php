d6e86f422266f28a1ae10e253411454b4c1d7de6
radhika4 <38878788+radhika4@users.noreply.github.com>
2023-02-03 20:06:38 +0530

    weather_rep added form to submit city value

---
+++ weather_rep.php
@@ -0,0 +1,7 @@
+<form method="post" action="#" id="new-form" 
+              enctype="multipart/form-data">
+	<h6>City</h6>
+                                                
+    <input type="text" name="city"  value="" id="city" placeholder="city..."/>
+    <input type="submit" name="btn_submit" value="Submit">                                            
+</form>
